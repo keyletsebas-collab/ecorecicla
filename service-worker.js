61c227238a5dfed6876d4539d68e3a454eda251c
EcclesiaSync Deployer <ecclesiasync@example.com>
2026-02-22 19:49:55 -0400

    Solucionar problema de cache: Actualizar Service Worker a v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reciminsa-v1';
+const CACHE_NAME = 'reciminsa-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -12,13 +12,25 @@ const ASSETS = [
   '/js/finance.js',
   '/js/settings.js',
   '/js/firebase-config.js',
+  '/js/pdf-utils.js',
   '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
