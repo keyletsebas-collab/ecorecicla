daff48009d8fc85e3a5c127db1cfd9b05da4fec5
Keylet Sebastian <keyletsebas@gmail.com>
2026-07-08 21:39:59 -0400

    Configure WEBVIEW2_USER_DATA_FOLDER in App.xaml.cs constructor to resolve C:\Program Files permission errors in .NET MAUI Windows builds

--- ReciminsaApp/App.xaml.cs
+++ ReciminsaApp/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace ReciminsaApp;
+namespace ReciminsaApp;
 
 public partial class App : Application
 {
 	public App()
 	{
+		// Set WebView2 user data folder to LocalApplicationData to avoid C:\Program Files permission issues on Windows
+		var userDataFolder = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "ReciminsaApp", "WebView2");
+		System.Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", userDataFolder);
+
 		InitializeComponent();
 
 		MainPage = new MainPage();
